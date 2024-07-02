df9bbb519908343d0e22bd8ced44bcca75c33b26
eekrwl <wani9909@naver.com>
2024-07-02 23:04:07 +0900

    240702

--- leetcode/q1079_Letter_Tile_Possibilities.cpp
+++ leetcode/q1079_Letter_Tile_Possibilities.cpp
@@ -1,1 +1,38 @@
-dd
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+int answer=0;
+int alpabet[26]={};
+void bt()
+{
+    for(int i=0;i<26;i++)
+    {
+        if(alpabet[i]>0)
+        {
+            alpabet[i]--;
+            answer++;
+            bt();
+            alpabet[i]++;
+        }
+    }
+}
+int numTilePossibilities(string tiles)
+{
+    for(int i=0;i<tiles.length();i++)
+    {
+        alpabet[tiles[i]-'A']++;
+    }
+
+    bt();
+}
+
+int main()
+{
+    string tiles = "AAABBC";
+   
+    numTilePossibilities(tiles);
+    cout << answer << endl;
+
+    return 0;
+}
